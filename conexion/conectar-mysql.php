431a3e08696dd9f10f6b1321a8a14a958d564d8f
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-18 22:29:16 -0600

    Add files via upload

---
+++ conexion/conectar-mysql.php
@@ -0,0 +1,14 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $nomBaseDatos = "project";
+    $password= "";
+    // Conectando a la base de datos
+    $conexion = mysqli_connect($servidor,$usuario,$password, $nomBaseDatos);
+    if($conexion){
+        #echo "si se pudo may :)";
+    }else{
+        #echo "no se pudp wey";
+    }
+
+?>
