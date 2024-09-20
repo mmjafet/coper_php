7a39644609c0bdc1ce220756e69e978a641c1147
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-19 22:30:37 -0600

    Add files via upload

---
+++ agregar_usuario.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+
+// Verifica si el usuario no ha iniciado sesión
+if (!isset($_SESSION['usuario']) || $_SESSION['rol'] != 1) {
+    // Redirige al usuario a la página de inicio de sesión
+    header("Location: login.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Usuario</title>
+    <!-- Incluye Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h2>Formulario de Registro</h2>
+        <form action="login/guardar_usuario.php" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="nombre">Nombre:</label>
+                <input type="text" class="form-control" id="nombre" name="nombre" required>
+            </div>
+            <div class="form-group">
+                <label for="ap_paterno">Apellido Paterno:</label>
+                <input type="text" class="form-control" id="ap_paterno" name="ap_paterno" required>
+            </div>
+            <div class="form-group">
+                <label for="ap_materno">Apellido Materno:</label>
+                <input type="text" class="form-control" id="ap_materno" name="ap_materno" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Contraseña:</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <input type="hidden" name="rol" value=1>
+            <button type="submit" class="btn btn-primary">Registrar</button>
+            <a href="usuarios.php" class="btn btn-secondary">Cancelar</a>
+        </form>
+    </div>
+
+    <!-- Incluye Bootstrap JS y dependencias -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
