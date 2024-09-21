d39e25615dbc6a132dc00e800d11b496a63a29dd
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2024-09-20 23:05:29 -0600

    Add files via upload

---
+++ categoria.php
@@ -0,0 +1,145 @@
+<?php
+session_start();
+
+// Verifica si el usuario no ha iniciado sesión
+if (!isset($_SESSION['usuario']) || $_SESSION['rol'] != 1) {
+    // Redirige al usuario a la página de inicio de sesión
+header("Location: login.php");
+exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Presentacion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        html, body {
+            height: 100%;
+            background-color: rgb(230, 234, 240);
+        }
+        .sidebar .nav-link,
+        .sidebar .btn-toggle {
+            color: #d1e7ff;
+        }
+        .sidebar .nav-link:hover,
+        .sidebar .btn-toggle:hover {
+            color: #fff;
+        }
+        .sidebar .btn-toggle:focus {
+            box-shadow: none;
+        }
+        .content {
+            flex-grow: 1;
+        }
+        .card-link {
+            text-decoration: none;
+            color: #495057;
+        }
+        .card-link:hover {
+            text-decoration: underline;
+            color: #007bff;
+        }
+    </style>
+</head>
+<body>
+    <!-- Barra lateral para navegación -->
+    <main class="d-flex flex-nowrap h-100">
+        <div class="d-flex flex-column flex-shrink-0 p-4 sidebar bg-dark m-3">
+            <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+                <span class="fs-4 text-white fa-solid fa-wine-bottle"> DEPOSITO</span>
+            </a>
+            <hr>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="mb-1">
+                    <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 text-white" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+                        <i class="fas fa-chevron-down me-2 text-"></i> Características generales
+                    </button>
+                    <div class="collapse show" id="home-collapse">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="presentacion.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">Presentación</a></li>
+                            <li><a href="categoria.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded ttext-dark fw-bold bg-white">Categoría</a></li>
+                            <li><a href="marca.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">Marca</a></li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li class="mb-1">
+                    <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed text-white" data-bs-toggle="collapse" data-bs-target="#comp-collapse" aria-expanded="false">
+                        <i class="fas fa-chevron-down me-2"></i> Compras
+                    </button>
+                    <div class="collapse" id="comp-collapse">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="mostrar_compras.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">Ver Compra</a></li>
+                            <li><a href="compras.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">Realizar compra</a></li>
+                            <li><a href="proveedor.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">Proveedores</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="mb-1">
+                    <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed text-white" data-bs-toggle="collapse" data-bs-target="#pro-collapse" aria-expanded="false">
+                        <i class="fas fa-chevron-down me-2"></i> Productos
+                    </button>
+                    <div class="collapse" id="pro-collapse">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="producto.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">Agregar producto</a></li>
+                            <li><a href="existencias.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">Existencias</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item"><a href="ver_ventas.php" class="nav-link text-white"><i class="fas fa-shopping-cart me-2 text-white"></i> Ver Ventas</a></li>
+                <li class="border-top my-3"></li>
+                <li class="nav-item"><a href="usuarios.php" class="nav-link text-white"><i class="fas fa-user-tie me-2 text-white"></i> Usuarios</a></li>
+            </ul>
+            <a href="cerrar_sesion.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <span class="fs-4 text-white btn btn-danger">Cerrar sesión</span>
+            </a>
+        </div>
+         <!-- Barra lateral para navegación END-->
+
+        <div class="content flex-grow-1 mt-4 p-4">
+            <h2 >Categoria</h2>
+            <a href='./Agregar/AgregarCategoria.php' class='btn btn-primary' id=''>Agregar nuevo</a>
+            <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <?php
+                include("./conexion/conectar-mysql.php");
+                $sql = 'SELECT * FROM categoria WHERE status="1"';
+                # Ejecutar consulta
+                $ejecConsulta = mysqli_query($conexion,$sql);
+                # Obtener datos de consulta
+
+                while($Pre = mysqli_fetch_array($ejecConsulta)){ 
+                        echo "<tr>
+                        <th scope='row'>" . $Pre[0] . "</th>
+                        <td>" . $Pre[1] . "</td>
+                        <td>" . $Pre[2] . "</td>
+                        <td>
+                        <a href='./Editar/Ecategoria.php?id=".$Pre[0]."' class='btn btn-info mg-20'>Editar</a>
+                        <a href='./Eliminar/Dcategoria.php?id=".$Pre[0]."' class='btn btn-danger mg-20'>Eliminar</a>
+                        
+                        </td>
+                        </tr>";
+                }
+                mysqli_close($conexion);
+                ?>
+            </tbody>
+            </table>
+        </div>
+
+    </section>
+
+    <script src="https://kit.fontawesome.com/af31879e24.js" crossorigin="anonymous"></script>
+    </main>
+</body>
+</html>
